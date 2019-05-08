a24650105ffe235d16cec8cbed4fcc81a685ab7a
Tiffany Nelson <tiffanynelson0424@gmail.com>
2019-05-07 19:18:10 -0500

    new post

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react"
+import { connect } from "react-redux"
+import { createPost } from "../actions/PostActions"
+import PropTypes from "prop-types"
 
 class PostForm extends Component {
   constructor(props) {
@@ -21,15 +24,7 @@ class PostForm extends Component {
       body: this.state.body,
     }
 
-    fetch("https://jsonplaceholder.typicode.com/posts", {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify(post),
-    })
-      .then((res) => res.json())
-      .then((data) => console.log(data))
+    this.props.createPost(post)
   }
 
   render() {
@@ -56,4 +51,11 @@ class PostForm extends Component {
   }
 }
 
-export default PostForm
+PostForm.propTypes = {
+  createPost: PropTypes.func.isRequired,
+}
+
+export default connect(
+  null,
+  { createPost }
+)(PostForm)
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -8,6 +8,12 @@ class Posts extends Component {
     this.props.fetchPosts()
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.newPost) {
+      nextProps.posts.unshift(nextProps.newPost) // unshift adds element to the front of array
+    }
+  }
+
   render() {
     const postItems = this.props.posts.map((post) => (
       <div key={post.id}>
@@ -29,8 +35,10 @@ Posts.propTypes = {
   posts: PropTypes.array.isRequired,
 }
 
+// takes the app state (from store), and returns object to be included in component's props
 const mapStateToProps = (state) => ({
   posts: state.posts.items, // reason for using state.posts is that in reducer we call it posts
+  newPost: state.posts.item,
 })
 
 export default connect(
